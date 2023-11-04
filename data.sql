4593c2eaf6f10d50a0cdcb5d07f3a1755ff9a53c
DevKaranJ <karan.joshi.dev@gmail.com>
2023-11-05 00:36:18 +0530

    "Add 'species' column to the 'animals' table"

--- data.sql
+++ data.sql
@@ -1,7 +1,12 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23),
-       ('Gabumon', '2018-11-15', 2, TRUE, 8),
-       ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-       ('Devimon', '2017-05-12', 5, TRUE, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23, 'Unknown'),
+       ('Gabumon', '2018-11-15', 2, TRUE, 8, 'Unknown'),
+       ('Pikachu', '2021-01-07', 1, FALSE, 15.04, 'Unknown'),
+       ('Devimon', '2017-05-12', 5, TRUE, 11, 'Unknown'),
+       ('Squirtle', '1993-04-02', 3, FALSE, -12.13, 'Unknown'),
+       ('Angemon', '2005-06-12', 1, TRUE, -45, 'Unknown'),
+       ('Boarmon', '2000-06-07', 7, TRUE, 20.4, 'Unknown'),
+       ('Blossom', '1998-10-13', 3, TRUE, 17, 'Unknown'),
+       ('Ditto', '2022-05-14', 4, TRUE, 22, 'Unknown');
--- queries.sql
+++ queries.sql
@@ -1,10 +1,46 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- Select animals with names ending in 'mon'
 SELECT * FROM animals WHERE name LIKE '%mon';
+
+-- Select names of animals born between 2016 and 2019
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- Select names of neutered animals with less than 3 escape attempts
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+-- Select the date of birth for animals with names 'Agumon' and 'Pikachu'
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+
+-- Select names and escape attempts of animals with weight over 10.5 kg
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+-- Select all neutered animals
 SELECT * FROM animals WHERE neutered = TRUE;
+
+-- Select all animals except 'Gabumon'
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
+
+-- Select animals with weight between 10.4 kg and 17.3 kg
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) 
+FROM animals 
+WHERE YEAR(date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY species;
