33de92fed6e5ead5f38e3620db9735e06055832d
Karan Joshi <Karan190213@gmail.com>
2023-11-07 20:27:08 +0530

    DATABASE UPDATED

--- queries.sql
+++ queries.sql
@@ -24,6 +24,57 @@ SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 -- Select animals with weight between 10.4 kg and 17.3 kg
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- PROJECT 2 --
+
+/* To start a transaction and update the animals specise column and then rollback the changes */
+ BEGIN;
+ UPDATE animals SET species = 'unspecified';
+ SELECT * FROM animals;
+ ROLLBACK;
+
+ /* Inside a transaction */
+ BEGIN;
+ /* 1: Update the animals table by setting the species column to digimon for all animals that have a name ending in mon */
+ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+/* 2: Update the animals table by setting the species column to pokemon for all animals that don't have species already set. */
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL OR species = '';
+
+/* Verify that changes were made. */
+SELECT * FROM animals;
+
+/* Commit the transaction. */
+COMMIT;
+
+/* Verify that changes persist after commit. */
+SELECT * FROM animals;
+
+/* Inside a transaction delete all records in the animals table, then roll back the transaction. */
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/* Inside a transaction: */
+BEGIN;
+/* 1:Delete all animals born after Jan 1st, 2022. */
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals;
+
+/* Create a savepoint for the transaction.*/
+SAVEPOINT SP1;
+
+/* Update all animals' weight to be their weight multiplied by -1 */
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+
+/* Update all animals' weights that are negative to be their weight multiplied by -1. */
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+/* Extra Queries */
+
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
 
--- schema.sql
+++ schema.sql
@@ -1,11 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Create the "animals" table
 CREATE TABLE animals (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
   escape_attempts INT NOT NULL,
   neutered BOOLEAN NOT NULL,
-  weight_kg DECIMAL(10,2) NOT NULL,
-  species VARCHAR(255) NOT NULL
+  weight_kg DECIMAL(10,2) NOT NULL
 );
+
+-- Add the "species" column to the "animals" table
+ALTER TABLE animals
+ADD species VARCHAR(255);
