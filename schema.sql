bb39adcedfcc65543beac6a5169fd0e5eb42f75f
Karan Joshi <Karan190213@gmail.com>
2023-11-08 15:27:12 +0530

    Updated the species table using REFERENCES clause

--- schema.sql
+++ schema.sql
@@ -27,13 +27,26 @@ CREATE TABLE species (
    name VARCHAR(255) NOT NULL
 );
 
--- Add a new column 'species_id' of type INT
+-- Add the 'owner_id' column to the 'animals' table.
+ALTER TABLE animals
+ADD owner_id INT;
+
+-- Establish the foreign key relationship for the 'owner_id' column.
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
+
+
+-- Add the 'species_id' column to the 'animals' table.
 ALTER TABLE animals
 ADD species_id INT;
 
--- Add a new column 'owner_id' of type INT
+-- Establish the foreign key relationship for the 'species_id' column.
 ALTER TABLE animals
-ADD owner_id INT;
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
 
 -- Drop the 'species' column
 ALTER TABLE animals
